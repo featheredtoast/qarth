290f17f909ad81017b64cb9f61dc9043a208b44f
Jeff Wong <awole20@gmail.com>
2018-05-20 21:15:52 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject qarth "0.1.3"
+(defproject qarth "0.2.0"
   :description "OAuth for serious people"
   :url "https://github.com/mthvedt/qarth"
   :license {:name "Eclipse Public License"
